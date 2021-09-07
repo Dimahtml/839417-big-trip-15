0767b66ce9c6b38b0a47d0a261af0e497dc90e8d
Dimahtml <bomabo@mail.ru>
2021-09-07 21:42:40 +0500

    1.5 Разобьёт код на модули

--- src/main.js
+++ src/main.js
@@ -1,9 +1,35 @@
 import {createTripInfoTemplate} from './view/trip-info.js';
+import {createSiteMenuTemplate} from './view/site-menu.js';
+import {createFilterTemplate} from './view/filter.js';
+import {createSortTemplate} from './view/sort.js';
+import {createEventsListTemplate} from './view/events-list-container.js';
+import {createEventTemplate} from './view/event.js';
+import {createTotalPriceTemplate} from './view/total-price.js';
+import {createEventEditTemplate} from './view/event-edit.js';
+
+const EVENT_COUNT = 3;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const tripMainElement = document.querySelector('.trip-main');
+const siteMenuElement = document.querySelector('.trip-controls__navigation');
+const siteFilterElement = document.querySelector('.trip-controls__filters');
+const tripEventsElement = document.querySelector('.trip-events');
 
-render(tripMainElement, createTripInfoTemplate(), 'beforeend');
+render(tripMainElement, createTripInfoTemplate(), 'afterbegin');
+render(siteMenuElement, createSiteMenuTemplate(), 'beforeend');
+render(siteFilterElement, createFilterTemplate(), 'beforeend');
+render(tripEventsElement, createSortTemplate(), 'beforeend');
+render(tripEventsElement, createEventsListTemplate(), 'beforeend');
+
+const eventsList = document.querySelector('.trip-events__list');
+const tripInfoElement = document.querySelector('.trip-main__trip-info');
+
+for (let i = 0; i < EVENT_COUNT; i++) {
+  render(eventsList, createEventTemplate(), 'beforeend');
+}
+
+render(tripInfoElement, createTotalPriceTemplate(), 'beforeend');
+render(eventsList, createEventEditTemplate(), 'afterbegin');
