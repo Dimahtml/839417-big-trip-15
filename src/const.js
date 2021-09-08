38b3397688a228fa7ff3e02ab2c1f10ebd5c8f90
Dimahtml <bomabo@mail.ru>
2021-09-08 22:47:33 +0500

    рефакторинг event-edit

---
+++ src/const.js
@@ -0,0 +1,12 @@
+export const Types = {
+  TAXI: 'taxi',
+  BUS: 'bus',
+  TRAIN: 'train',
+  SHIP: 'ship',
+  TRANSPORT: 'transport',
+  DRIVE: 'drive',
+  FLIGHT: 'flight',
+  CHECKIN: 'check-in',
+  SIGHTSEEING: 'sightseeing',
+  RESTARAUNT: 'restaurant',
+};
