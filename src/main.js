bb2b64b0c09d80c5b08d69aed736baa33b04d4f0
Dimahtml <bomabo@mail.ru>
2021-09-09 13:26:23 +0500

    3.1.1 Отрефакторит рендер-метод

--- src/main.js
+++ src/main.js
@@ -8,37 +8,38 @@ import {createTotalPriceTemplate} from './view/total-price.js';
 import {createEventEditTemplate} from './view/event-edit.js';
 import {createNoEventTemplate} from './view/no-event.js';
 import {generateEvent} from './mock/event.js';
+import {renderTemplate} from './utils.js';
 
 const EVENT_COUNT = 10;
 
 const events = new Array(EVENT_COUNT).fill().map(generateEvent);
 
-const render = (container, template, place = 'beforeend') => {
-  container.insertAdjacentHTML(place, template);
-};
+// const renderTemplate = (container, template, place = 'beforeend') => {
+//   container.insertAdjacentHTML(place, template);
+// };
 
 const tripMainElement = document.querySelector('.trip-main');
 const siteMenuElement = document.querySelector('.trip-controls__navigation');
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
-render(siteMenuElement, createSiteMenuTemplate());
-render(siteFilterElement, createFilterTemplate());
+renderTemplate(siteMenuElement, createSiteMenuTemplate());
+renderTemplate(siteFilterElement, createFilterTemplate());
 
 if (events.length === 0) {
-  render(tripEventsElement, createNoEventTemplate());
+  renderTemplate(tripEventsElement, createNoEventTemplate());
 } else {
-  render(tripMainElement, createTripInfoTemplate(), 'afterbegin');
-  render(tripEventsElement, createSortTemplate());
-  render(tripEventsElement, createEventsListTemplate());
+  renderTemplate(tripMainElement, createTripInfoTemplate(), 'afterbegin');
+  renderTemplate(tripEventsElement, createSortTemplate());
+  renderTemplate(tripEventsElement, createEventsListTemplate());
 
   const eventsList = document.querySelector('.trip-events__list');
   const tripInfoElement = document.querySelector('.trip-main__trip-info');
 
   for (let i = 1; i < EVENT_COUNT; i++) {
-    render(eventsList, createEventTemplate(events[i]));
+    renderTemplate(eventsList, createEventTemplate(events[i]));
   }
 
-  render(tripInfoElement, createTotalPriceTemplate());
-  render(eventsList, createEventEditTemplate(events[0]), 'afterbegin');
+  renderTemplate(tripInfoElement, createTotalPriceTemplate());
+  renderTemplate(eventsList, createEventEditTemplate(events[0]), 'afterbegin');
 }
