3a7c9bb3a13c4f0610cce0c64fb62a294b3a36a0
Dimahtml <bomabo@mail.ru>
2021-09-11 11:28:46 +0500

    перенес отрисовку маршрута в презентер. пока еще не открываются точки

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import SiteMenuView from './view/site-menu.js';
 import FilterView from './view/filter.js';
-import SortView from './view/sort.js';
-import EventsContainer from './view/trip.js';
-import EventView from './view/event.js';
-import EventEditView from './view/event-edit';
-import NoEventView from './view/no-event.js';
+// import SortView from './view/sort.js';
+// import EventsContainer from './view/events-list.js';
+// import EventView from './view/event.js';
+// import EventEditView from './view/event-edit';
+// import NoEventView from './view/no-event.js';
+
+import TripPresenter from './presenter/trip.js';
+
 import {generateEvent} from './mock/event.js';
 import {render, RenderPosition} from './utils/render.js';
 
@@ -16,54 +19,57 @@ const siteMenuElement = document.querySelector('.trip-controls__navigation');
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
-const renderEvent = (eventListElement, event) => {
-  const eventComponent = new EventView(event);
-  const eventEditComponent = new EventEditView(event);
+// const renderEvent = (eventListElement, event) => {
+//   const eventComponent = new EventView(event);
+//   const eventEditComponent = new EventEditView(event);
 
-  const openPoint = () => {
-    eventListElement.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
-  };
+//   const openPoint = () => {
+//     eventListElement.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+//   };
 
-  const closePoint = () => {
-    eventListElement.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
-  };
+//   const closePoint = () => {
+//     eventListElement.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+//   };
 
-  const onEscKeyDown = (evt) => {
-    if (evt.key === 'Escape' || evt.key === 'Esc') {
-      evt.preventDefault();
-      closePoint();
-      document.removeEventListener('keydown', onEscKeyDown);
-    }
-  };
+//   const onEscKeyDown = (evt) => {
+//     if (evt.key === 'Escape' || evt.key === 'Esc') {
+//       evt.preventDefault();
+//       closePoint();
+//       document.removeEventListener('keydown', onEscKeyDown);
+//     }
+//   };
 
-  eventComponent.setOpenButtonClickHandler(() => {
-    openPoint();
-    document.addEventListener('keydown', onEscKeyDown);
-  });
+//   eventComponent.setOpenButtonClickHandler(() => {
+//     openPoint();
+//     document.addEventListener('keydown', onEscKeyDown);
+//   });
 
-  eventEditComponent.setCloseButtonClickHandler(() => {
-    closePoint();
-    document.removeEventListener('keydown', onEscKeyDown);
-  });
+//   eventEditComponent.setCloseButtonClickHandler(() => {
+//     closePoint();
+//     document.removeEventListener('keydown', onEscKeyDown);
+//   });
 
-  eventEditComponent.setFormSubmitHandler(() => {
-    closePoint();
-    document.removeEventListener('keydown', onEscKeyDown);
-  });
+//   eventEditComponent.setFormSubmitHandler(() => {
+//     closePoint();
+//     document.removeEventListener('keydown', onEscKeyDown);
+//   });
 
-  render(eventListElement, eventComponent.getElement(), RenderPosition.BEFOREEND);
-};
+//   render(eventListElement, eventComponent.getElement(), RenderPosition.BEFOREEND);
+// };
 
 render(siteMenuElement, new SiteMenuView().getElement(), RenderPosition.BEFOREEND);
 render(siteFilterElement, new FilterView().getElement(), RenderPosition.BEFOREEND);
 
-if (events.length > 0) {
-  render(tripEventsElement, new SortView().getElement(), RenderPosition.BEFOREEND);
-  render(tripEventsElement, new EventsContainer().getElement(), RenderPosition.BEFOREEND);
-  const eventsList = document.querySelector('.trip-events__list');
-  for (let i = 0; i < EVENT_COUNT; i++) {
-    renderEvent(eventsList, events[i]);
-  }
-} else {
-  render(tripEventsElement, new NoEventView().getElement(), RenderPosition.BEFOREEND);
-}
+// if (events.length > 0) {
+//   render(tripEventsElement, new SortView().getElement(), RenderPosition.BEFOREEND);
+//   render(tripEventsElement, new EventsContainer().getElement(), RenderPosition.BEFOREEND);
+//   const eventsList = document.querySelector('.trip-events__list');
+//   for (let i = 0; i < EVENT_COUNT; i++) {
+//     renderEvent(eventsList, events[i]);
+//   }
+// } else {
+//   render(tripEventsElement, new NoEventView().getElement(), RenderPosition.BEFOREEND);
+// }
+
+const tripPresenter = new TripPresenter(tripEventsElement);
+tripPresenter.init(events);
--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -1,36 +1,69 @@
-import SortView from './view/sort.js';
-import TripView from './view/trip.js';
-import EventView from './view/event.js';
-import EventEditView from './view/event-edit';
-import NoEventView from './view/no-event.js';
+import SiteMenuView from '../view/site-menu.js';
+import FilterView from '../view/filter.js';
+import SortView from '../view/sort.js';
+import EventsListView from '../view/events-list.js';
+import EventView from '../view/event.js';
+import EventEditView from '../view/event-edit.js';
+import NoEventView from '../view/no-event.js';
 import {render, RenderPosition} from '../utils/render.js';
 
 export default class Trip {
   constructor(tripContainer) {
     this._tripContainer = tripContainer;
 
-    this._tripComponent = new TripView();
+    this._siteMenuComponent = new SiteMenuView();
+    this._filterComponent = new FilterView();
+    this._eventsListComponent = new EventsListView();
     this._sortComponent = new SortView();
     this._noEventComponent = new NoEventView();
   }
 
   init(tripEvents) {
     this._tripEvents = tripEvents.slice();
-    // Метод для инициализации (начала работы) модуля,
-    // малая часть текущей функции renderBoard в main.js
+
+    this._renderTrip();
   }
 
   _renderSort() {
-    // Метод для рендеринга сортировки
+    render(this._tripContainer, this._sortComponent, RenderPosition.AFTERBEGIN);
   }
 
-  _renderEvent() {
-    // Метод, куда уйдёт логика созданию и рендерингу компонетов задачи,
-    // текущая функция renderTask в main.js
-  }
+  _renderEvent(event) {
+    const eventComponent = new EventView(event);
+    const eventEditComponent = new EventEditView(event);
 
-  _renderEvents() {
-    // Метод для рендеринга N-задач за раз
+    const openPoint = () => {
+      eventListElement.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+    };
+
+    const closePoint = () => {
+      eventListElement.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        closePoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    eventComponent.setOpenButtonClickHandler(() => {
+      openPoint();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    eventEditComponent.setCloseButtonClickHandler(() => {
+      closePoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    eventEditComponent.setFormSubmitHandler(() => {
+      closePoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    render(this._eventsListComponent, eventComponent.getElement(), RenderPosition.BEFOREEND);
   }
 
   _renderNoEvents() {
@@ -38,7 +71,13 @@ export default class Trip {
   }
 
   _renderTrip() {
-    // Метод для инициализации (начала работы) модуля,
-    // бОльшая часть текущей функции renderBoard в main.js
+    if (this._tripEvents.length > 0) {
+      this._renderSort();
+      render(this._tripContainer, this._eventsListComponent, RenderPosition.BEFOREEND);
+      // const eventsList = document.querySelector('.trip-events__list');
+      this._tripEvents.forEach((event) =>  this._renderEvent(event));
+    } else {
+      this._renderNoEvents();
+    }
   }
 }
