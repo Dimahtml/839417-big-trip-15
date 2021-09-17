fc23f0ce564639f4baba56ebfe4c9b424955f620
Dimahtml <bomabo@mail.ru>
2021-09-17 18:11:06 +0500

    7.1.9 Добавит презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import SiteMenuView from './view/site-menu.js';
-import FilterView from './view/filter.js';
 import TripPresenter from './presenter/trip.js';
+import FilterPresenter from './presenter/filter.js';
 import PointsModel from './model/points.js';
 import FilterModel from './model/filter.js';
 import {generatePoint} from './mock/point.js';
@@ -9,13 +9,6 @@ import {render, RenderPosition} from './utils/render.js';
 const POINT_COUNT = 10;
 
 const points = new Array(POINT_COUNT).fill().map(generatePoint);
-
-const filters = [
-  {
-    type: 'everything',
-    name: 'EVERYTHING',
-  },
-];
 
 const pointsModel = new PointsModel();
 pointsModel.setPoints(points);
@@ -28,7 +21,9 @@ const siteFilterElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
 render(siteMenuElement, new SiteMenuView(), RenderPosition.BEFOREEND);
-render(siteFilterElement, new FilterView(filters, 'everything'), RenderPosition.BEFOREEND);
 
 const tripPresenter = new TripPresenter(tripEventsElement, pointsModel);
+const filterPresenter = new FilterPresenter(siteFilterElement, filterModel, pointsModel);
+
+filterPresenter.init();
 tripPresenter.init();
