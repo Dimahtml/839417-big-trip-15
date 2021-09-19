7a6682c29eef9e78ad3d9269ba16cf0d6b91d49d
Dimahtml <bomabo@mail.ru>
2021-09-19 20:07:15 +0500

    8.1.2 Добавит модуль Api в точке входа. WIP

--- src/main.js
+++ src/main.js
@@ -4,13 +4,12 @@ import TripPresenter from './presenter/trip.js';
 import FilterPresenter from './presenter/filter.js';
 import PointsModel from './model/points.js';
 import FilterModel from './model/filter.js';
-// import {generatePoint} from './mock/point.js';
 import {remove, render, RenderPosition} from './utils/render.js';
 import {MenuItem} from './const.js';
-import {points} from '../src/mock/point.js';
+import {points as mockPoints} from '../src/mock/point.js';
 
 const pointsModel = new PointsModel();
-pointsModel.setPoints(points);
+pointsModel.setPoints(mockPoints);
 
 const filterModel = new FilterModel();
 
@@ -21,7 +20,20 @@ const siteFilterElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 const addNewPointElement = document.querySelector('.trip-main__event-add-btn');
 const siteMenuComponent = new SiteMenuView();
-// const statisticsComponent = new StatisticsView(points);
+import Api from './api.js';
+
+const AUTHORIZATION = 'Basic qwerjfaskdlfftiye';
+const END_POINT = 'https://15.ecmascript.pages.academy/big-trip';
+
+const api = new Api(END_POINT, AUTHORIZATION);
+
+api.getPoints().then((points) => {
+  console.log(points);
+  // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+  // а ещё на сервере используется snake_case, а у нас camelCase.
+  // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+  // Есть вариант получше - паттерн "Адаптер"
+});
 
 render(siteMenuElement, siteMenuComponent, RenderPosition.BEFOREEND);
 
@@ -42,7 +54,7 @@ const handleSiteMenuClick = (menuItem) => {
       siteMenuComponent.getElement().querySelector('#site-menu-stats').classList.add('trip-tabs__btn--active');
       siteMenuComponent.getElement().querySelector('#site-menu-table').classList.remove('trip-tabs__btn--active');
       tripPresenter.destroy();
-      statisticsComponent = new StatisticsView(points);
+      statisticsComponent = new StatisticsView(mockPoints);
       render(pageMainContainerElement, statisticsComponent, RenderPosition.BEFOREEND);
       break;
   }
